62105ae31a17dcf116232f48d3fbed75f151ce9e
Bjorn De Craemer <bjorn.decraemer@yahoo.com>
2019-12-17 21:11:54 +0100

    Added worker for automatic steering

---
+++ src/main/java/com/bjornspetprojects/heatingbe/core/services/schedule/ScheduledWorker.java
@@ -0,0 +1,58 @@
+package com.bjornspetprojects.heatingbe.core.services.schedule;
+
+import com.bjornspetprojects.heatingbe.core.services.HeatingService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledWorker {
+
+    private final HeatingService heatingService;
+
+    public ScheduledWorker(HeatingService heatingService) {
+        this.heatingService = heatingService;
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void checkForChanges(){
+        if(heatingService.getManualStatus()){
+            boolean currentBurnerStatus = heatingService.getBurnerStatus();
+            boolean currentPumpStatus = heatingService.getPumpStatus();
+            long currentTemp = heatingService.getTempReading();
+            int requestedTemp = heatingService.getRequestedTemp();
+
+            if(shouldBurnerBeActive(currentTemp,requestedTemp,currentBurnerStatus)){
+                heatingService.activateHeating();
+            }
+            else{
+                heatingService.disactivateHeating();
+            }
+
+            if(shouldPumpBeActive(currentTemp,requestedTemp,currentPumpStatus)){
+                heatingService.activatePump();
+            }
+            else{
+                heatingService.disactivatePump();
+            }
+        }
+    }
+
+    private boolean shouldBurnerBeActive(long currentTemp, Integer requestedTemp, boolean burnerStatus){
+        if(requestedTemp == null || requestedTemp == 0){
+            return false;
+        }
+        return currentTemp < requestedTemp;
+    }
+
+    private boolean shouldPumpBeActive(long currentTemp, Integer requestedTemp, boolean pumpStatus){
+        if(requestedTemp <= 0){
+            return false;
+        }
+        if(pumpStatus){
+            return currentTemp <= requestedTemp;
+        }
+        else{
+            return currentTemp < requestedTemp;
+        }
+    }
+}
